35da4fa129aeb4f4a66df2d20115fc0f64bf98ec
yongbin <supermania@gmail.com>
2010-06-19 14:22:54 +0900

    - initilize for Catalyst::Manual

---
+++ perl/site_perl/Catalyst/Manual/Plugins.pm
@@ -0,0 +1,21 @@
+=head1 NAME
+
+Catalyst::Manual::Plugins - See Catalyst::Manual::Components instead 
+
+=head1 SEE INSTEAD
+
+L<Catalyst::Manual::Components>
+
+=cut
+
+=head1 AUTHORS
+
+Catalyst Contributors, see Catalyst.pm
+
+=head1 COPYRIGHT
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
